780f5aff2475d457aacea8340073fa51eb04837b
Carlos Alexandre <carlos24106@outlook.com>
2023-12-10 16:24:28 -0300

    Header finish

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Header from "./Components/Header/Index";
+
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/Components/Header/Index.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="bg-dark-purple p-4">
+      <a href="/" className="text-light-white text-center block font-bold text-2xl">
+        Substrack
+      </a>
+    </header>
+  );
+};
+
+export default Header;
