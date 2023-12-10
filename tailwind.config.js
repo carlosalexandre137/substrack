0b6c35a48e212aaf0e788a696c156c431db648a0
Carlos Alexandre <carlos24106@outlook.com>
2023-12-10 16:23:55 -0300

    Color settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          purple: "#451952",
+          black: "#181616",
+          brown: "#662549",
+        },
+        light: {
+          white: "#FFFBF5",
+          pink: "#E2CFCF",
+          purple: "#74647C",
+          green: "#A4DB5E",
+        },
+      },
+    },
   },
   plugins: [],
 };
