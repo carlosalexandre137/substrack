0854d8aaaced56e55f8c1d8f5033f36d9cbe44be
Carlos Alexandre <carlos24106@outlook.com>
2024-01-14 17:49:27 -0300

    Fixing header

--- src/Components/Header/Index.js
+++ src/Components/Header/Index.js
@@ -2,23 +2,26 @@ import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="bg-dark-purple p-4 shadow-md shadow-light-purple text-center">
+    <header className="bg-dark-purple p-4 shadow-md shadow-light-purple text-center sticky top-0 w-full z-40">
       <div className="container flex justify-between items-center mx-auto">
         <Link to="/" className="text-light-white font-bold text-2xl">
           Substrack
         </Link>
         <ul className="flex gap-5">
           <li>
-            <NavLink to="/" className={({ isActive }) => `text-light-white font-bold text-md ` + (isActive ? "active" : "")}>
-              HOME
+            <NavLink
+              to="/"
+              className={({ isActive }) => `font-bold text-md uppercase ` + (isActive ? "text-light-green" : "text-light-white")}
+            >
+              Home
             </NavLink>
           </li>
           <li>
             <NavLink
               to="/assinaturas/registrar"
-              className={({ isActive }) => `text-light-white font-bold text-md uppercase ` + (isActive ? "active" : "")}
+              className={({ isActive }) => `font-bold text-md uppercase ` + (isActive ? "text-light-green" : "text-light-white")}
             >
-              registrar
+              Registrar
             </NavLink>
           </li>
         </ul>
