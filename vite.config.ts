a5483f3a37b45b78979d0b0e899761da552bd7c7
Carlos Alexandre <carlos24106@outlook.com>
2024-02-18 15:09:58 -0300

    Add config build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
